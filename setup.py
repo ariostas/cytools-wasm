d0e970ff5abf042e6880bcec974e3e51577a0787
Andres Rios Tascon <ariostas@gmail.com>
2023-12-23 11:56:16 -0500

    Deleted some files and updated readme

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="cytools",
-    version="1.0.11",
-    author="Liam McAllister Group",
+    name="cytools-wasm",
+    version="0.0.1",
+    author="Andres Rios Tascon",
     author_email="",
-    description="A software package for analyzing Calabi-Yau hypersurfaces in toric varieties.",
+    description="A WebAssembly implementation of CYTools.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/LiamMcAllisterGroup/cytools",
+    url="https://github.com/ariostas/cytools-wasm",
     packages=setuptools.find_packages(),
     license="GNU General Public License (GPL)",
-    python_requires='>=3.6',
-    install_requires=[], #["numpy", "scipy", "cvxopt", "flint-py", "pplpy", "ortools"],
+    python_requires='>=3.9',
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License (GPL)",
